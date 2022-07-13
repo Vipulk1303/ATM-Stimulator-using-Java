de70c462d90cbf671a991b7c853145d3e45ac36c
Vipulk1303 <49546322+Vipulk1303@users.noreply.github.com>
2022-07-13 16:15:27 +0530

    Add files via upload

---
+++ ATM-Simulator-System-master/ATM-Simulator-System/src/ASimulatorSystem/Conn.java
@@ -0,0 +1,19 @@
+package ASimulatorSystem;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Typeyourpassword");
+            s =c.createStatement();
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
